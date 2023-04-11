31afeebfa10d67988309957e78c4308fc0f98176
Vivien Le Duc <leducvivien@gmail.com>
2023-04-11 17:28:03 +0200

    updated README

---
+++ bash.sh
@@ -0,0 +1,7 @@
+echo "# template-generator-core" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vivienld-templates/template-generator-core.git
+git push -u origin main
